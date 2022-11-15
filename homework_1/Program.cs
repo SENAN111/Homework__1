b4caead69b1a40d3a4b8acf564b397390d069610
SENAN111 <behbudovsenan0@gmail.com>
2022-11-15 22:32:19 +0400

    first commit

---
+++ homework_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("\t\t\t\t\t\tEdedi daxil edin!");
+int eded=Convert.ToInt32(Console.ReadLine());
+int mertebe = 0;
+int maks_mertebe = 0;
+if (eded==0)
+{
+    Console.WriteLine("Maks mertebe : 0");
+}
+else
+{
+    while(eded!=0)
+    {
+        mertebe = eded % 10;
+        if (mertebe>maks_mertebe)
+        {
+            maks_mertebe=mertebe;
+        }
+        eded=eded/10;
+    }
+    Console.WriteLine("Maks mertebe:" + "" + maks_mertebe);
+      
+}
